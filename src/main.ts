cfa9bdc5cdb317e683912f351c61114a424339f7
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-07 15:17:18 +0200

    task 4

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.scss';
 
 const task1 = 2;
 const task2 = "hello";
+const number = 5;
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <h1>Exercise: PB-Console</h1>
@@ -9,7 +10,15 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <h2>Task 1</h2>
 <p>${task1}</p>
 
-<h2>Task 1</h2>
+<h2>Task 2</h2>
 <p>${task2}</p>
+
+<h2>Task 3</h2>
+<p>${number}</p>
+
+<h2>Task 4</h2>
+<p>${typeof number}</p>
+
+
 
 `
