612e15f3aefac259ee88cb6616265846fb787d9c
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-07 16:11:38 +0200

    with map

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,27 @@ const isOnline = true;
 
 const firstName = 'Jake';
 const lastName = 'Rolanding';
-const city = 'Paris';
+const city = 'Berlin';
 const favoriteBand = 'Hatari';
 const favoriteMovie = 'Blue Velvet';
-const favoriteBook = 'Jenseits von Gut und Böse'
+const favoriteBook = 'Jenseits von Gut und Böse';
+
+// task 7 - display a list of colors
+const colors = ['red', 'blue', 'green', 'orange', 'purple', 'tan', 'pink'];
+let colorHtml = '';
+//for-of
+// for (const color of colors) {
+// 	colorHtml += `<li style="color: ${color}">${color}</li>`;
+// }
+
+//for
+// for (let i = 0; i <= colors.length - 1; i++) {
+// 	const color = colors[i];
+// 	colorHtml += `<li>${color}</li>`;
+// }
+
+// forEach
+// colors.filter(m => m.length <= 6).forEach(m => colorHtml += `<li>${m}</li>`);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <h1>Exercise: PB-Console</h1>
@@ -39,8 +56,11 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 	<li>${favoriteMovie}</li>
 	<li>${favoriteBook}</li>
 </ul>
-<p>My name is ${firstName} ${lastName}, I live in ${city}, my favorite band is ${favoriteBand}, my favorite movie is ${favoriteMovie}, and my favorite book is ${favoriteBook}.</p>
+<p><em>My name is ${firstName} ${lastName}, I live in ${city}, my favorite band is ${favoriteBand}, my favorite movie is ${favoriteMovie}, and my favorite book is ${favoriteBook}.</em></p>
 
-
-
+<h2>Task 7</h2>
+<p>There are ${colors.length} colors.</p>
+<ul>
+	<!-- ${colors.map(m => `<li>${m}</li>`).join('')} -->
+</ul>
 `
